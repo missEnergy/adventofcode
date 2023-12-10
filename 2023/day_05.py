65d1ad9a6d92c75ad56946ccb5eaae510a36189c
Ellissa  Verseput <ellissa.verseput@sympower.net>
2023-12-10 16:14:03 +0100

    part 5b

--- 2023/day_05.py
+++ 2023/day_05.py
@@ -15,38 +15,17 @@ for map in data[1:]:
     maps.append(tmp_map)
 
 # part A
-# find the lowest location number that corresponds to any of the initial seeds.
-# locations = []
-# for s in seeds:
-#     for m in maps:
-#         for r in m:
-#             if r["source"] <= s < r["source"] + r["length"]:
-#                 s = r["dest"] + s - r["source"]
-#                 break
-#     locations.append(s)
-# locations.sort()
-
-# submit(locations[0], part="a", day=day, year=year)
-
-# part B
-seed_ranges = []
-for i in range(0, len(seeds), 2):
-    seed_ranges.append({"seed_start": seeds[i], "seed_range": seeds[i+1]})
-
-# find the lowest location number that corresponds to any of the initial seeds.
 locations = []
-for s_r in seed_ranges:
-    seed_start = s_r["seed_start"]
-    seed_range = s_r["seed_range"]
+for s in seeds:
     for m in maps:
         for r in m:
-            if r["source"] <= seed_start < r["source"] + r["length"]:
-                if r["source"] <= seed_start + seed_range - 1 < r["source"] + r["length"]:
-                    seed_start = r["dest"] + seed_start - r["source"]
-                    break
-                else:
-                    raise Exception("doesn't fit in range")
-    locations.append(seed_start)
+            if r["source"] <= s < r["source"] + r["length"]:
+                s = r["dest"] + s - r["source"]
+                break
+    locations.append(s)
 locations.sort()
 
-# submit(answer_b, part="b", day=day, year=year)
+submit(locations[0], part="a", day=day, year=year)
+
+# part B
+# GOOGLE SHEET BABY!
