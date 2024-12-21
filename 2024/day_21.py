5292cd07a3f66c2a93d442250ac7caddc87e63e6
Ellissa  Verseput <ellissa.verseput@sympower.net>
2024-12-21 13:50:02 +0000

    day 21 init

--- 2024/day_21.py
+++ 2024/day_21.py
@@ -1,7 +1,7 @@
 from aocd import get_data, submit
 
 year = 2024
-day =
+day = 21
 
 data = get_data(year=year, day=day)
 lines = data.splitlines()
@@ -10,6 +10,6 @@ for line in lines:
     print(line)
 
 # PART A
-submit(answer, part="a", day=day, year=year)
+# submit(answer, part="a", day=day, year=year)
 # PART B
-submit(answer, part="b", day=day, year=year)
+# submit(answer, part="b", day=day, year=year)
