8b018d9d4df795e41133160ec3972302d374a0e1
Ellissa  Verseput <ellissa.verseput@sympower.net>
2024-12-22 16:12:59 +0000

    day 17 complete whoho, happy with hack part B ;p

--- 2024/day_17.py
+++ 2024/day_17.py
@@ -1,15 +1,49 @@
-from aocd import get_data, submit
-
 year = 2024
 day = 17
 
-data = get_data(year=year, day=day)
-lines = data.splitlines()
-
-for line in lines:
-    print(line)
-
 # PART A
-# submit(answer, part="a", day=day, year=year)
+A = 64854237
+B = 0
+C = 0
+
+while True:
+    B = A % 8
+    B = B ^ 1
+    C = int(A/(pow(2,B)))
+    B = B ^ 5
+    B = B ^ C
+    print(f"{B%8},", end='') # OUT
+    A = int(A/(pow(2,3)))
+    if A == 0:
+        print('done')
+        break
+
 # PART B
-# submit(answer, part="b", day=day, year=year)
+ref = "2,4,1,1,7,5,1,5,4,0,5,5,0,3,3,0,"
+init = 164279024967600 # update the init when you find partial match and refine from there
+while True:
+    print(init)
+    A = init
+    B = 0
+    C = 0
+    out = ''
+    while True:
+        B = A % 8
+        B = B ^ 1
+        C = int(A/(pow(2,B)))
+        B = B ^ 5
+        B = B ^ C
+        out += f"{B%8},"
+        A = int(A/(pow(2,3)))
+        if A == 0:
+            break
+        if len(out) >= len(ref):
+            break
+    if out[0:] == ref[0:]: # start with looking for only end of seq match, and then make larger
+        print('done!')
+        print(f"ref: {ref}")
+        print(f"out: {out}")
+        break
+    init += 1 # start with bigger steps (e.g. 1000000000)
+    print(f"ref: {ref}")
+    print(f"out: {out}")
